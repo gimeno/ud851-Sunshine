792c444187910bb0bf5ae011bf0b14cc06c37d20
Roberto Gimeno <r.gimenocallejas@gmail.com>
2017-11-08 23:21:54 +0000

    Exercise 01.02 solved

---
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/data/WeatherData.java
@@ -0,0 +1,24 @@
+package com.example.android.sunshine.data;
+
+/**
+ * Class to fake weather data
+ * Created by Roberto on 08/11/2017.
+ */
+public class WeatherData {
+
+    public static String[] getfAKEWeatherData() {
+        return new String[] {
+                "Monday 26º",
+                "Tuesday 26º",
+                "Wednesday 26º",
+                "Thursday 26º",
+                "Friday 26º",
+                "Saturday 26º",
+                "Sunday 26º",
+                "Sunday 26º",
+                "Sunday 26º",
+                "Sunday 26º",
+                "Sunday 26º"
+        };
+    }
+}
